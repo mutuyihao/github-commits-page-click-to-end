527df70621788d8bf1f1898534b9e5223e05422d
mutuyihao <96763582+mutuyihao@users.noreply.github.com>
2025-02-24 15:56:45 +0800

    feat:update

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 // @name         Github commits最后一页
 // @namespace    http://tampermonkey.net/
 // @version      0.2
-// @description  try to take over the world!
-// @author       You
+// @description  This will help users get a button to click to end of the github commits page.
+// @author       Mutu
 // @match        https://github.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        none
+// @license      MIT
+// @downloadURL https://update.greasyfork.org/scripts/470176/Github%20commits%E6%9C%80%E5%90%8E%E4%B8%80%E9%A1%B5.user.js
+// @updateURL https://update.greasyfork.org/scripts/470176/Github%20commits%E6%9C%80%E5%90%8E%E4%B8%80%E9%A1%B5.meta.js
 // ==/UserScript==
 
 (function () {
@@ -31,40 +34,24 @@
     }, 1000);
 
     function getCommits() {
-        const spanList = document.getElementsByTagName('span')
-        let commits
-        for (let ele of spanList) {
-            let index = ele.innerText.indexOf('Commits')
-            if (index > 0) {
-                commits = ele.innerText.slice(0, index).replace(',', '')
-                break
-            }
-        }
-        if (commits > 0) {
+        if (document.querySelector("span .fgColor-default")) {
+            let commits = document.querySelector("span .fgColor-default").innerText
             sessionStorage.setItem("commits", commits)
+            console.log(commits)
+        } else {
+            console.log('Fail to find out commit number')
         }
     }
+
     function insertBtn() {
         let commitsStr = sessionStorage.getItem("commits")
-        let btnGroup = getBtnGroup()
-        let btnToNext = btnGroup.lastElementChild
+        let btnToNext = document.querySelector('a[data-testid=pagination-next-button]')
+        let btnGroup = btnToNext.parentNode.parentNode
         let btnToEnd = document.createElement('a')
-        btnToEnd.className = btnToNext.classList.value
+        btnToEnd.className = btnToNext.classList
         let commitsNum = parseInt(commitsStr.replace(/,/, ""), 10)
         btnToEnd.href = btnToNext.href.replace(/\+\d+/g, `+${commitsNum - 34}`)
         btnToEnd.innerText = "Click To End"
         btnGroup.appendChild(btnToEnd)
-    }
-    function getBtnGroup() {
-        const spanList = document.getElementsByTagName('span')
-        let temp;
-        for (let ele of spanList) {
-
-            if (ele.innerText == 'Next') {
-                temp = ele
-                break
-            }
-        }
-        return temp.parentElement.parentElement
     }
 })();
